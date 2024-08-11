607e156485c0abb4e97f59f7a981f547b7e6c96a
Mohit Kumar <118979618+Mohitkumarpushkar@users.noreply.github.com>
2024-08-11 21:33:09 +0530

    Update orderController.js

--- controller/orderController.js
+++ controller/orderController.js
@@ -9,7 +9,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
     // frontend url
-    const frontend_url = "http://localhost:5173";
+   
 
     try {
         const newOrder = new orderModel({
